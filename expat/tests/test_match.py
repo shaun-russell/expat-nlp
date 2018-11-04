338643b8dee938c5440af0021203794e8419b0f2
Shaun Russell <shaun@ensemblemusic.co.nz>
2018-11-04 17:50:10 +1300

    restructured everything, started adding tests

---
+++ expat/tests/test_match.py
@@ -0,0 +1,25 @@
+''' Tests for the match.py file. '''
+import unittest
+
+# write tests for the following
+class TestMatching(unittest.TestCase):
+  # 1. Match startswith PASS
+  def test_startswith_pass(self):
+    assertEquals()
+  # 2. Match startswith FAIL
+  # 3. Match endswith PASS
+  # 4. Match endswith FAIL
+  # 5. Match exact PASS
+  # 6. Match exact FAIL
+  # 7. Match contains PASS
+  # 8. Match contains FAIL
+  # 9. Match surround PASS
+  # 10. Match surround FAIL
+  # 11. Match mixed PASS 1
+  # 12. Match mixed PASS 2
+  # 13. Match mixed FAIL 1
+  # 14. Match mixed FAIL 2
+  # 15. Match case PASS 1
+  # 16. Match case PASS 1
+  # 17. Match case FAIL 1
+  # 18. Match case FAIL 2
