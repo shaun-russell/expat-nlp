d9d346f11ba413c46f3ec1f027913203295244a6
Shaun Russell <shaun@ensemblemusic.co.nz>
2018-11-05 17:24:22 +1300

    parsing works when reading a filepath

--- expat/core/parse.py
+++ expat/core/parse.py
@@ -1,81 +1,16 @@
-# todo
-# read xml file
-# create pattern, group, and word classes
-# populate defaults
+''' Classes and functions for parsing data sources (like XML) '''
 
 import xml.etree.cElementTree as etree
-# import lxml
-
-# use this as a placeholder for properties
-NULL = None
+from core.structures import PatternGroup
 
 class Parser():
   @staticmethod
-  def parse_patterns(source):
+  def parse_patterns(source, is_filepath=False):
     tree = None
-    if isinstance(source, str):
-      # source is a string, use string parse
-     tree = etree.fromstring(source.strip())
+    if is_filepath:
+      tree = etree.parse(source).getroot()
     else:
-      # assume file so read from that
-      tree = etree.parse(source)
+     tree = etree.fromstring(source.strip())
     # build the entire structure through the PatternGroup constructor
     patterns = PatternGroup(tree)
     return patterns
-
-class PatternGroup():
-  def __init__(self, tree):
-    self.label = tree.find('label')
-    self.version = tree.find('version')
-    # pattern object for every child element
-    self.patterns = [Pattern(child) for child in tree.getchildren()]
-
-class Pattern():
-  ''' Represents a pattern of words. '''
-  def __init__(self, tree):
-    self.name = tree.get('name')
-    self.description = tree.get('description')
-    # 'class' is a protected word in python, so the field is 'classname'
-    self.classname = tree.get('class')
-    self.priority = int(tree.get('priority'))
-    self.label = tree.get('label')
-    # create the correct object for the child elements of the pattern
-    self.children = [self._parse_contents(el) for el in tree.getchildren()]
-
-  def _parse_contents(self, element):
-    if element.tag == 'word':
-      return Word(element)
-    elif element.tag == 'wordgroup':
-      return WordGroup(element)
-    # if it's not a word and word group, throw an exception rather than
-    # try to continue quietly.
-    else:
-      raise ValueError('Element is not a word or wordgroup.') 
-
-class WordGroup():
-  def __init__(self, tree):
-    self.min = int(tree.get('min'))
-    self.max = int(tree.get('max'))
-    self.label = tree.get('label')
-    self.words = [Word(element) for element in tree.getchildren()]
-
-class Word():
-  def __init__(self, tree):
-    self.min = int(tree.get('min'))
-    self.max = int(tree.get('max'))
-    self.is_contextual = bool(tree.get('contextual'))
-    self.label = tree.get('label')
-    # pos
-    self.pos = tree.get('pos')
-    self.excluded_pos = tree.get('expos')
-    # dependencies
-    self.dependencies = tree.get('deps')
-    self.excluded_dependencies = tree.get('exdeps')
-    self.num_dependencies_needed = tree.get('depnum')
-    # types
-    self.types = tree.get('type')
-    self.excluded_types = tree.get('extype')
-    self.num_types_needed = tree.get('typenum')
-    # words and lemmas
-    self.word = tree.get('word')
-    self.lemma = tree.get('lemma')
--- expat/core/structures.py
+++ expat/core/structures.py
@@ -1,5 +1,7 @@
 ''' Common structures to pass data around. '''
 
+# Many of these class descriptions aren't very useful. At some point,
+# write better definitions.
 
 class ListAttributes():
   def __init__(self, to_find, to_exclude, required_num):
@@ -14,3 +16,70 @@ class ListAttributes():
     # if nothing specified, nothing to exclude
     self.to_exclude = to_exclude.split(',')
     self.has_exclusions = to_exclude != ''
+
+
+class PatternGroup():
+  ''' A collection of patterns. '''
+  def __init__(self, tree):
+    self.label = tree.find('label')
+    self.version = tree.find('version')
+    # pattern object for every child element
+    self.patterns = [Pattern(child) for child in tree.getchildren()]
+
+class Pattern():
+  ''' Represents a sequence of words that represent a certain pattern. '''
+  def __init__(self, tree):
+    self.name = tree.get('name')
+    self.description = tree.get('description')
+    # 'class' is a protected word in python, so the field is 'classname'
+    self.classname = tree.get('class')
+    self.priority = int(tree.get('priority'))
+    self.label = tree.get('label')
+    # create the correct object for the child elements of the pattern
+    self.children = [Pattern.get_correct_class(el) for el in tree.getchildren()]
+
+  @staticmethod
+  def get_correct_class(element):
+    if element.tag == 'word':
+      return PatternWord(element)
+    elif element.tag == 'wordgroup':
+      return WordGroup(element)
+    # if it's not a word and word group, throw an exception rather than
+    # try to continue quietly.
+    else:
+      raise ValueError('Element is not a word or wordgroup.') 
+
+class WordGroup():
+  ''' A group that contains multiple pattern words. This allows matching of words
+      only if N of those words occur. Allows structures like ( word1 word2 ) | (word3). '''
+  def __init__(self, tree):
+    # min and max are the number of word matches needed for the whole
+    # group to match. If only 1 word matches and 2 are required, the 
+    # entire group fails to match.
+    self.min = int(tree.get('min'))
+    self.max = int(tree.get('max'))
+    self.label = tree.get('label')
+    self.words = [Pattern.get_correct_class(element) for element in tree.getchildren()]
+
+class PatternWord():
+  # this description is terrible, change this at some point
+  ''' The attributes of a word that is part of a pattern definition. '''
+  def __init__(self, tree):
+    self.min = int(tree.get('min'))
+    self.max = int(tree.get('max'))
+    self.is_contextual = bool(tree.get('contextual'))
+    self.label = tree.get('label')
+    # pos
+    self.pos = tree.get('pos')
+    self.excluded_pos = tree.get('expos')
+    # dependencies
+    self.dependencies = tree.get('deps')
+    self.excluded_dependencies = tree.get('exdeps')
+    self.num_dependencies_needed = tree.get('depnum')
+    # types
+    self.types = tree.get('type')
+    self.excluded_types = tree.get('extype')
+    self.num_types_needed = tree.get('typenum')
+    # words and lemmas
+    self.word = tree.get('word')
+    self.lemma = tree.get('lemma')
