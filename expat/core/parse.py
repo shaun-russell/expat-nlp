1fd16cd49a5db50195af75f2c6d704347143475e
Shaun Russell <shaun@ensemblemusic.co.nz>
2019-01-07 17:09:23 +1300

    cleaned and commented parse.py

--- expat/core/parse.py
+++ expat/core/parse.py
@@ -4,8 +4,10 @@ import xml.etree.cElementTree as etree
 from core.structures import PatternGroup
 
 class Parser():
+  ''' Parser for Pattern xml files. '''
   @staticmethod
   def parse_patterns(source, is_filepath=False):
+    ''' Converts an XML file into a PatternGroup object. '''
     tree = None
     if is_filepath:
       tree = etree.parse(source).getroot()
@@ -16,8 +18,14 @@ class Parser():
     return patterns
 
 class ExtensionParser():
+  ''' Reader for extension files (type annotation). '''
+
   @staticmethod
   def parse(extensionfile):
+    ''' Returns a dictionary with type_id and extension item attributes. '''
+    # This gets fed into another function.
+    # Could be improved by making it return a class object, rather than
+    # just a mystery tuple.
     extensions = {}
     for line in extensionfile:
       if not line.startswith('#') and len(line) > 3:
